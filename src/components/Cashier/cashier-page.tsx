e0c37c035bf9c0f2ba54b1781186f99d69911094
Nelson Neculhueque <Nelson2005abner@gmail.com>
2024-09-06 23:13:27 -0400

    fix(cashier): cambios en seccion main del cashier

--- src/components/Cashier/cashier-page.tsx
+++ src/components/Cashier/cashier-page.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'wouter';
 import { Button } from '../ui/button';
 import useStore from '@/store/useAuthStore';
+import { Label } from '../ui/label'; 
+import {Input} from '../ui/input' 
 
 const CajeroLayout = () => {
   const { user } = useStore();
@@ -49,43 +51,53 @@ const CajeroLayout = () => {
         {/* Main Content Section */}
         <main className="flex-1 ml-64 p-6 flex flex-col">
           <section className="bg-white shadow-md rounded-lg p-6 border border-gray-200 mb-6 flex-grow min-h-[400px]">
-            <h3 className="text-lg font-semibold mb-4">Productos en la Compra</h3>
-            <div className="text-gray-700 text-lg">Aquí se mostrarán los productos que se vayan ingresando.</div>
+            <h3 className="text-lg font-semibold mb-4">
+              Productos en la Compra
+            </h3>
+            <div className="text-gray-700 text-lg">
+              Aquí se mostrarán los productos que se vayan ingresando.
+            </div>
           </section>
 
-          <section className="bg-white shadow-md rounded-lg p-4 border border-gray-200 flex flex-col" style={{ minHeight: '120px' }}>
-            <h3 className="text-lg font-semibold mb-4">Código de Producto y Cantidad</h3>
+          <section
+            className="bg-white shadow-md rounded-lg p-4 border border-gray-200 flex flex-col"
+            style={{ minHeight: "120px" }}
+          >
+            <h3 className="text-lg font-semibold mb-4">
+              Ingresar Producto
+            </h3>
             <div className="flex gap-4">
-              {/* Campo de Código de Producto */}
-              <div className="flex items-center">
-                <label htmlFor="productCode" className="text-gray-700 w-1/3 text-right pr-4">Código del Producto:</label>
-                <input 
-                  id="productCode"
-                  type="text"
-                  className="border border-gray-300 rounded-lg p-2 flex-1"
-                  placeholder="Ingrese el código del producto"
+              <div className="flex-col items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Código
+                </Label>
+                <Input
+                  id="name"
+                  placeholder="001"
+                  className="col-span-3"
+                  required
                 />
               </div>
-              {/* Campo de Cantidad */}
-              <div className="flex items-center">
-                <label htmlFor="quantity" className="text-gray-700 w-1/3 text-right pr-4">Cantidad:</label>
-                <input 
-                  id="quantity"
-                  type="number"
-                  className="border border-gray-300 rounded-lg p-2 flex-1"
-                  placeholder="Ingrese la cantidad"
+              <div className="flex-col items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Cantidad
+                </Label>
+                <Input
+                  id="number"
+                  defaultValue={1}
+                  className="col-span-3"
                 />
               </div>
             </div>
             <div className="flex justify-end gap-4 mt-4">
-              <Button 
+              <Button
                 size="lg"
                 className="bg-blue-700 text-white hover:bg-blue-800 active:bg-blue-900 rounded-lg shadow-md transition duration-200"
               >
                 Confirmar
               </Button>
-              <Button 
-                variant="secondary" 
+              <Button
+                variant="secondary"
                 size="lg"
                 className="bg-gray-700 text-white hover:bg-gray-800 active:bg-gray-900 rounded-lg shadow-md transition duration-200"
               >
