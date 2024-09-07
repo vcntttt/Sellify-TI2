7fe23a94ff5aa0b46d764c5e26e64664613da9f7
Patricio Benavides <patojavier1981@gmail.com>
2024-09-07 12:54:44 -0400

    arreglos esquemas
    arreglos esquemas

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -1,114 +1,102 @@
 import { z } from "zod";
+import { categories } from "@/data/categories";
 
+export const productSchema = z.object({
+  id: z.number(),
+  name: z.string().min(2).max(50),
+  stock: z.number(),
+  price: z.number(),
+  category: z.enum(categories),
+  createdAt: z.date(),
+  dueDate: z.date(),
+  discount: z.number().min(0).max(100),
+});
 
+export const usuarioSchema = z.object({
+  id: z.number(),
+  email: z.string().email(),
+  rut: z.string().min(1).max(12),
+  password: z.string().min(6).max(50), // para los clientes usamos el rut noma
+  name: z.string().min(1).max(50),
+  apellido: z.string().min(1).max(50),
+});
 
 export const formaPagoSchema = z.object({
-    id: z.number(),
-    nombre: z.string().min(1).max(50),
-  });
-  
+  id: z.number(),
+  nombre: z.string().min(1).max(50),
+});
 
-  export const descuentosSchema = z.object({
-    id: z.number(),
-    id_producto: z.number(),
-    porcentaje: z.number().min(0).max(100),  
-    fecha_inicial: z.date(),
-    fecha_final: z.date(),
-  });
+export const descuentosSchema = z.object({
+  id: z.number(),
+  id_producto: z.number(),
+  porcentaje: z.number().min(0).max(100),
+  fecha_inicial: z.date(),
+  fecha_final: z.date(),
+});
 
-  export const categoriaSchema = z.object({
-    id: z.number(),
-    nombre: z.string().min(1).max(50),
-    cantidad: z.number().min(0),  
-  });
-  
- 
-  export const productosSchema = z.object({
-    id: z.number(),
-    nombre: z.string().min(1).max(50),
-    precio: z.number().min(0),  
-    fechaAgregado: z.date(),
-    vencimiento: z.date(),
-    categoria_id: z.number(),  
-  });
-  
- 
-  export const precioSchema = z.object({
-    id: z.number(),
-    fecha_inicial: z.date(),
-    fecha_final: z.date(),
-  });
-  
+export const categoriaSchema = z.object({
+  id: z.number(),
+  nombre: z.string().min(1).max(50),
+  cantidad: z.number().min(0),
+});
 
-  export const usuarioSchema = z.object({
-    id: z.number(),
-    correo: z.string().email(),  
-    rut: z.string().min(1).max(12),  
-    password: z.string().min(6).max(50),  
-    nombre: z.string().min(1).max(50),
-    apellido: z.string().min(1).max(50),
-  });
-  
+export const precioSchema = z.object({
+  id: z.number(),
+  fecha_inicial: z.date(),
+  fecha_final: z.date(),
+});
 
-  export const historialPuntosSchema = z.object({
-    id: z.number(),
-    id_cliente: z.number(),  
-    cantidad: z.number().min(0), 
-    fecha: z.date(),
-  });
-  
+export const historialPuntosSchema = z.object({
+  id: z.number(),
+  id_cliente: z.number(),
+  cantidad: z.number().min(0),
+  fecha: z.date(),
+});
 
-  export const tipoRegistroSchema = z.object({
-    id: z.number(),
-    descripcion: z.string().min(1).max(50),
-  });
-  
- 
-  export const detallePagoSchema = z.object({
-    id: z.number(),
-    id_producto: z.number(), 
-    id_formaPago: z.number(),  
-    total: z.number().min(0),  
-  });
-  
+export const tipoRegistroSchema = z.object({
+  id: z.number(),
+  descripcion: z.string().min(1).max(50),
+});
 
-  export const registroSchema = z.object({
-    id: z.number(),
-    descripcion: z.string().min(1).max(50),
-    id_tipoRegistro: z.number(),  
-    id_cajero: z.number(),        
-    id_detalleRegistro: z.number(),  
-    id_pago: z.number(),            
-  });
-  
+export const detallePagoSchema = z.object({
+  id: z.number(),
+  id_producto: z.number(),
+  id_formaPago: z.number(),
+  total: z.number().min(0),
+});
 
-  export const detalleRegistroSchema = z.object({
-    id: z.number(),
-    descripcion: z.string().min(1).max(50),
-    fecha: z.date(),
-    cantidad: z.number().min(0),  
-    id_tipoRegistro: z.number(),  
-    id_producto: z.number(),     
-  });
-  
+export const registroSchema = z.object({
+  id: z.number(),
+  descripcion: z.string().min(1).max(50),
+  id_tipoRegistro: z.number(),
+  id_cajero: z.number(),
+  id_detalleRegistro: z.number(),
+  id_pago: z.number(),
+});
 
-  export const stockSchema = z.object({
-    id: z.number(),
-    id_producto: z.number(),  
-    cantidad: z.number().min(0),  
-  });
-  
+export const detalleRegistroSchema = z.object({
+  id: z.number(),
+  descripcion: z.string().min(1).max(50),
+  fecha: z.date(),
+  cantidad: z.number().min(0),
+  id_tipoRegistro: z.number(),
+  id_producto: z.number(),
+});
 
-  export const puntosSchema = z.object({
-    id: z.number(),
-    id_cliente: z.number(),  
-    cantidad: z.number().min(0),  
-  });
-  
- 
-  export const tipoUsuarioSchema = z.object({
-    id: z.number(),
-    id_usuario: z.number(),  
-    tipo: z.string().min(1).max(50),
-  });
+export const stockSchema = z.object({
+  id: z.number(),
+  id_producto: z.number(),
+  cantidad: z.number().min(0),
+});
 
+export const puntosSchema = z.object({
+  id: z.number(),
+  id_cliente: z.number(),
+  cantidad: z.number().min(0),
+});
+
+export const tipoUsuarioSchema = z.object({
+  id: z.number(),
+  id_usuario: z.number(),
+  tipo: z.string().min(1).max(50),
+});
--- src/types.d.ts
+++ src/types.d.ts
@@ -2,29 +2,26 @@ import { productosSchema } from "./schemas/schemas";
 import { formaPagoSchema } from "./schemas/schemas";
 import { descuentosSchema } from "./schemas/schemas";
 import { categoriaSchema } from "./schemas/schemas";
-import { precioSchema } from "./schemas/schemas";
 import { usuarioSchema } from "./schemas/schemas";
 import { historialPuntosSchema } from "./schemas/schemas";
 import { tipoRegistroSchema } from "./schemas/schemas";
 import { detalleRegistroSchema } from "./schemas/schemas";
 import { registroSchema } from "./schemas/schemas";
-import { stockSchema } from "./schemas/schemas";
 import { puntosSchema } from "./schemas/schemas";
-import { tipoUsuarioSchema } from "./schemas/schemas";
 import { detallePagoSchema } from "./schemas/schemas";
 
+export type Productos = z.infer<typeof productosSchema>
+export type Usuario = z.infer<typeof usuarioSchema>
+export type FormaPago = z.infer<typeof formaPagoSchema>
+export type Descuentos= z.infer<typeof descuentosSchema>
+export type Categoria = z.infer<typeof categoriaSchema>
+export type HistorialPuntos = z.infer<typeof historialPuntosSchema>
+export type TipoRegistro = z.infer<typeof tipoRegistroSchema>
+export type DetalleRegistro= z.infer<typeof detalleRegistroSchema>
+export type Registro = z.infer<typeof registroSchema>
+export type Puntos = z.infer<typeof puntosSchema>
+export type TipoRegistro = z.infer<typeof tipoRegistroSchema>
+export type DetallePago = z.infer<typeof detallePagoSchema>
 
-export type productosSchema = z.infer<typeof productosSchema>
-export type formaPagoSchema = z.infer<typeof formaPagoSchema>
-export type descuentosSchema = z.infer<typeof descuentosSchema>
-export type categoriaSchema = z.infer<typeof categoriaSchema>
-export type precioSchema = z.infer<typeof precioSchema>
-export type usuarioSchema = z.infer<typeof usuarioSchema>
-export type historialPuntosSchema = z.infer<typeof historialPuntosSchema>
-export type tipoRegistroSchema = z.infer<typeof tipoRegistroSchema>
-export type detalleRegistroSchema= z.infer<typeof detalleRegistroSchema>
-export type registroSchema = z.infer<typeof registroSchema>
-export type stockSchema = z.infer<typeof stockSchema>
-export type puntosSchema = z.infer<typeof puntosSchema>
-export type tipoRegistroSchema = z.infer<typeof tipoRegistroSchema>
-export type detallePagoSchema = z.infer<typeof detallePagoSchema>
+// export type precio = z.infer<typeof precioSchema>
+// export type stock = z.infer<typeof stockSchema>
