e91b959784db2aaf3a5511079f7044deb1160930
TsuKenZo <128194754+TsuKenZo@users.noreply.github.com>
2024-09-12 10:57:03 -0300

    perf(custom-hooks): se agrego un hook para el manejo de productos

--- src/components/admin/products/products-page.tsx
+++ src/components/admin/products/products-page.tsx
@@ -1,22 +1,10 @@
 import AdminSection from "@/components/admin/Section";
-import {  useEffect, useState } from "react";
-import { products as productsData } from "@/data/products";
-import { Productos as ProductType } from "@/types";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
-
-async function fetchProducts(){
-  return productsData;
-}
+import { useProduct } from "@/hooks/products/useProducts";
 
 export default function Products() {
-  const [products, setProducts] = useState<ProductType[]>([]);
-  
-  useEffect(() => {
-    fetchProducts().then((data) => {
-      setProducts(data);
-    });
-  }, []);
+  const { products } = useProduct();
 
   return (
     <AdminSection title="Productos">
