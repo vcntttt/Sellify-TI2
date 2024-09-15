4a0b969214fb7d32455ed4d12b638f68806475c0
Patricio Benavides <patojavier1981@gmail.com>
2024-09-15 01:27:49 -0300

    update files

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,18 +1,45 @@
 import { z } from "zod";
-import { categories } from "@/data/categories";
 
 export const productSchema = z.object({
   id: z.number(),
-  name: z.string().min(2).max(50),
-  stock: z.preprocess((val) => (typeof val === 'string' ? parseInt(val, 10) : val), z.number().min(0)),
-  price: z.preprocess((val) => (typeof val === 'string' ? parseInt(val, 10) : val), z.number().min(0)),
-  category: z.enum(categories),
-  // createdAt: z.date(),
-  // dueDate: z.date(),
-  discount: z.object({
-    value: z.preprocess((val) => (typeof val === 'string' ? parseInt(val, 10) : val), z.number().min(0).max(100)),
-    dueDate: z.date({ required_error: "Debe ingresar una fecha de vencimiento de descuento" })
-  })
+  name: z
+    .string()
+    .min(2, { message: "El nombre del producto no puede estar vacío" }),
+  stock: z.preprocess(
+    (val) => (typeof val === "string" ? parseInt(val, 10) : val),
+    z.number().min(0)
+  ),
+  price: z.preprocess(
+    (val) => (typeof val === "string" ? parseInt(val, 10) : val),
+    z.number().min(0)
+  ),
+  category: z.string(),
+  createdAt: z.date(),
+  dueDate: z.date(),
+  discount: z
+    .object({
+      value: z.preprocess(
+        (val) => (typeof val === "string" ? parseInt(val, 10) : val),
+        z.number().min(0).max(100)
+      ),
+      dueDate: z
+        .date({
+          required_error: "Debe ingresar una fecha de vencimiento de descuento",
+        })
+        .nullable()
+        .optional(),
+    })
+    .optional()
+    .refine((discount) => {
+      if (discount?.value && discount.value > 0) {
+        return discount.dueDate !== null && discount.dueDate !== undefined;
+      }
+      return true;
+    }),
+});
+
+export const categorySchema = z.object({
+  name: z.string().min(1, { message: "El nombre de la categoria no puede estar vacío" }),
 });
 
 export const userSchema = z.object({
