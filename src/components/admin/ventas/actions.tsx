18fc0e3e2358b18d9ab58a35383f36b0bd1bb83c
Patricio Benavides <patojavier1981@gmail.com>
2024-09-18 23:58:07 -0300

    actualizacion tabla ventas
    actualizacion ventas/actions, columns, data-table

--- src/components/admin/ventas/actions.tsx
+++ src/components/admin/ventas/actions.tsx
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
-export default function VentasActions() {
+export default function VentasActions({ tableRef }: { tableRef: any }) {
+  const [date, setDate] = useState<Date | undefined>(undefined);
+
+  useEffect(() => {
+    if (date) {
+      tableRef.getColumn("fecha")?.setFilterValue(date);
+    } else {
+      tableRef.getColumn("fecha")?.setFilterValue(undefined);
+    }
+  }, [date, tableRef]);
+
   return (
     <div className="flex gap-4">
-    <Button>Accion</Button>
-  </div>
-  )
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={"outline"}
+            className={cn(
+              "w-[280px] justify-start text-left font-normal",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date ? format(date, "PPP") : <span>Filtrar por fecha</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
 }
