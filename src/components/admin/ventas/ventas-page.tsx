d3b07755299a077b24495cb1efaf2e1f5d634671
TsuKenZo <128194754+TsuKenZo@users.noreply.github.com>
2024-09-19 13:41:42 -0300

    perf(hook): hook para ventas y datos ventas

--- src/components/admin/ventas/ventas-page.tsx
+++ src/components/admin/ventas/ventas-page.tsx
@@ -1,12 +1,13 @@
 import AdminSection from "../section-template";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
-import { ventas } from "@/data/ventas";
+import { useVentasStore } from "@/store/use-ventas";
 
 
 
 export default function DemoPage() {
 
+  const {ventas} = useVentasStore();
     return (
         <AdminSection title="Ventas">
           <DataTable columns={columns} data={ventas} />
