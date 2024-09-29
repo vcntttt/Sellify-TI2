fe5262dc155bde072a4e79234515639070b04a27
TsuKenZo <128194754+TsuKenZo@users.noreply.github.com>
2024-09-28 21:35:30 -0300

    feat(hook): se agrego el hook para realizar el fetching de datos

---
+++ src/hooks/use-fetch.ts
@@ -0,0 +1,26 @@
+import axios from "@/api/axios";
+import { useState, useEffect } from "react";
+
+export function useFetch(url: string) {
+  const [data, setData] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<any>(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        setError(error);
+        console.error("There was a problem with the axios operation:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [url]);
+
+  return { data, isLoading, error };
+}
