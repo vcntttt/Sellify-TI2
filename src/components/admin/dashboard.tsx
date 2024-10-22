3140cbee89532ace61fd1167894ddd8de5130e01
Vicente Rivera <vicenterivera225@gmail.com>
2024-10-22 19:26:40 -0300

    feat: dashboard responsive

--- src/components/admin/dashboard.tsx
+++ src/components/admin/dashboard.tsx
@@ -3,11 +3,11 @@ import AdminSection from "@/components/admin/section-template";
 export default function Dashboard() {
   return (
     <AdminSection title="Panel de Administración" border={false}>
-      <div className="grid grid-cols-3 grid-rows-3 gap-2 h-full w-full *:border-slate-700/20 *:border-[1px] *:p-4 *:rounded-md">
-        <div className="col-span-2"></div>
-        <div className="row-span-3"></div>
-        <div className="col-span-2"></div>
-        <div className="col-span-2"></div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 grid-rows-3 gap-2 h-full w-full *:border-slate-700/20 *:border-[1px] *:p-4 *:rounded-md">
+        <div className="lg:col-span-2"></div>
+        <div className="lg:row-span-3"></div>
+        <div className="lg:col-span-2"></div>
+        <div className="lg:col-span-2"></div>
       </div>
     </AdminSection>
   );
