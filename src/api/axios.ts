6c0e7a93e0dbd8f6d44b9384cdb3175e8fec985d
Vicente Rivera <vicenterivera225@gmail.com>
2024-10-22 17:50:38 -0300

    fix: conexion con api en entorno de produccion

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,27 +1,28 @@
 import axios from "axios";
-// import { useAuthStore } from "../store/authStore";
 
 const instance = axios.create({
-  baseURL: "/api/",
-  withCredentials: true,
-});
+    baseURL: process.env.NODE_ENV === "production" 
+      ? "http://170.239.85.88:5000"
+      : "/api/",
+    withCredentials: true,
+  });
 
-/* instance.interceptors.request.use(config => {
-    const token  = useAuthStore.getState().token;
-    config.headers = {
-        authorization: `Bearer ${token}`,
-    }
-    return config
-})
+//   instance.interceptors.request.use((config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers["Authorization"] = `Bearer ${token}`;
+//     }
+//     return config;
+//   });
 
-instance.interceptors.response.use(
-    response => response,
-    error => {
-        if (error.response.status === 401) {
-            useAuthStore.getState().logOut();
-        }
-        return Promise.reject(error)
-    }
-) */
+// instance.interceptors.response.use(
+//     response => response,
+//     error => {
+//         if (error.response.status === 401) {
+//             useAuthStore.getState().logOut();
+//         }
+//         return Promise.reject(error)
+//     }
+// )
 
 export default instance;
